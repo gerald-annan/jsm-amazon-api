a69569498bd5cd3543704b985392bd242f04e527
Gerald Annan <annang2@stanbic.com.gh>
2022-01-29 13:17:40 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -45,6 +45,20 @@ app.get('/products/:productId/reviews', async (req, res) => {
 });
 
 // Get product offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+    const api_key = key;
+    
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+// Get search results
 
 
 app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
